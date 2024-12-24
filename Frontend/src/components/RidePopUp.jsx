278285f6362cfa3c1e952f9f6de02ddea1800293
HEMANT-YADAV123 <hy700793@gmail.com>
2024-12-24 18:58:14 +0530

    ride popup panel connected with ignore button

--- Frontend/src/components/RidePopUp.jsx
+++ Frontend/src/components/RidePopUp.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Customer from '../assets/Customer.png'
 
-const RidePopUp = () => {
+const RidePopUp = (props) => {
   return (
     <div>
-      <h5 onClick={()=>{}} className='p-1 text-center w-[93%] absolute top-0'><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
+      <h5 onClick={()=>{props.setRidePopUpPanel(false)}} className='p-1 text-center w-[93%] absolute top-0'><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
             <h3 className='text-2xl font-semibold mb-5'>New Ride Available!</h3>
             <div className='flex items-center justify-between p-3 bg-yellow-400 rounded-lg mt-4'>
                 <div className='flex items-center gap-3 '>
@@ -41,7 +41,7 @@ const RidePopUp = () => {
               <button onClick={()=>{}} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>
                   Confirm
               </button>
-              <button onClick={()=>{}} className='w-full  bg-gray-300 text-gray-700 font-semibold p-2 rounded-lg'>
+              <button onClick={()=>{props.setRidePopUpPanel(false)}} className='w-full  bg-gray-300 text-gray-700 font-semibold p-2 rounded-lg'>
                   Ignore
               </button>
             </div>
--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import map from '../assets/map.png.png'
 import logo from '../assets/CityCruise__3_-removebg-preview.png'
-
 import { Link } from 'react-router-dom'
+import {useGSAP} from '@gsap/react'//gsap is an animation library and useGSAP ias a hook used for gsap
+import gsap from 'gsap'
 import CaptainDetails from './CaptainDetails'
 import RidePopUp from '../components/RidePopUp'
 
+
 const CaptainHome = () => {
+
+const [ridePopUpPanel,setRidePopUpPanel] = useState(true);
+const ridePopUpPanelRef = useRef(null);
+
+useGSAP(function(){
+  if(ridePopUpPanel)
+  {
+    gsap.to(ridePopUpPanelRef.current,{
+      transform: 'translateY(0)'
+    })
+  }
+  else
+  {
+    gsap.to(ridePopUpPanelRef.current,{
+      transform: 'translateY(100%)'
+    })
+  }
+},[ridePopUpPanel])
   return (
     <div className='h-screen'>
         <div className='fixed p-6 top-0 w-screen flex items-center justify-between'>
@@ -21,9 +41,9 @@ const CaptainHome = () => {
       <div className='h-2/5 p-6'>
           <CaptainDetails/>
       </div>
-
-      <div className='fixed w-full z-10 bottom-0 bg-white px-3 py-10 pt-12'>
-           <RidePopUp/>
+      {/* Ride popup panel */}
+      <div ref={ridePopUpPanelRef} className='fixed w-full z-10 translate-y-full bottom-0 bg-white px-3 py-10 pt-12'>
+           <RidePopUp setRidePopUpPanel={setRidePopUpPanel}/>
       </div>
     </div>
   )
