194855a8a2ee08b6cc31e75447e3df09ef0bccb4
HEMANT-YADAV123 <hy700793@gmail.com>
2024-12-26 14:04:05 +0530

    suggestion route is also created

--- Backend/routes/mapsRoutes.js
+++ Backend/routes/mapsRoutes.js
@@ -9,4 +9,17 @@ router.get('/get-coordinates',
     authMiddleware.authUser,
     mapController.getCoordinates);
 
+router.get('/get-distance-time',
+    query('origin').isString().isLength({min: 3}),
+    query('destination').isString().isLength({min: 3}),
+    authMiddleware.authUser,
+    mapController.getDistanceTime
+)    
+
+router.get('/get-suggestions',
+    query('input').isString().isLength({min: 3}),
+    authMiddleware.authUser,
+    mapController.getAutoCompleteSuggestions
+)
+
 module.exports = router;
